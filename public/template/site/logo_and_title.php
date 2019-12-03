36f3f0eb7b4d7244fdbd0d28b0d0a2c0835eddff
MohammedSallam <mosallam06@gmail.com>
2019-12-03 21:38:10 +0200

    change color

--- public/template/site/logo_and_title.php
+++ public/template/site/logo_and_title.php
@@ -3,9 +3,9 @@
         <img class="img-responsive" src="<?= $this->route->baseUrl() . IMAGES_PATH . 'logo.png'?>" alt="">
     </a>
 </div>
-<div class="bg-success text-light text-center logo_and_title">
+<div class="text-light text-center logo_and_title">
     <a href="<?= $this->route->baseUrl() ?>">
-        <span  class="mr-3">الوحدة الإلكترونية</span>
+        <span  class="mr-3">أكاديمية التأهيل و التدريب وخدمة الزائرات النسائية</span>
     </a>
 </div>
 <div class="text-center my-3 font-weight-bold">إشراف : أكاديمية المسجد النبوي النسائية</div>
